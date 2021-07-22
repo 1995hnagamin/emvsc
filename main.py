8849ade2c89a4ff3df06adcfbe3b58885e3fa9d3
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-07-22 17:20:50 +0900

    Implement Lax-Wendroff scheme

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import wx
 import numpy as np
+import itertools
 
 import matplotlib as mpl
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 import matplotlib.animation as mplanim
 
+import vlasov
 class Plot(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
         self.figure = mpl.figure.Figure(figsize=(2, 2))
         self.canvas = FigureCanvasWxAgg(self, -1, self.figure)
-        self.animation = mplanim.FuncAnimation(self.figure, self.plot, interval=25)
+        self.animation = mplanim.FuncAnimation(self.figure, self.plot, interval=50)
         self.ax = self.figure.add_subplot(111)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.canvas, 1, wx.EXPAND)
         self.SetSizer(sizer)
 
-        self.x = np.linspace(-np.pi, 3*np.pi, 500)
+        resolution = 100
+        self.xmax = 100.
+        self.x = np.linspace(0, self.xmax, resolution)
+        width = 5.
+        center = self.xmax / 2
+        f_init = np.exp(-((self.x-center)/width)**2)
+        values = vlasov.adv1d(
+            system_length=self.xmax,
+            velocity=1.,
+            init=f_init,
+            ngrid=resolution,
+            dt=.1)
+        self.gen = itertools.islice(values, 0, None, 40)
 
-    def plot(self, i):
-        dx = 0.1
+    def plot(self, _):
+        f = next(self.gen)
+
         self.ax.cla()
-        self.ax.plot(self.x, np.sin(self.x - i*dx))
+        self.ax.set_xlim(0, self.xmax)
+        self.ax.set_ylim(-0.5, 1.5)
+        self.ax.plot(self.x, f)
 
 if __name__ == "__main__":
     app = wx.App()
--- vlasov.py
+++ vlasov.py
@@ -1,4 +1,14 @@
-import filter
+import numpy as np
 
-def lax_wendroff_fl(limiter):
-    return
+def lax_wendroff(f, courant):
+    return f \
+        - courant/2 * (np.roll(f, -1) - np.roll(f, 1)) \
+        + courant**2/2 * (np.roll(f, -1) - 2*f + np.roll(f, 1))
+
+def adv1d(*, system_length, velocity, init, ngrid, dt):
+    f = init.copy()
+    dx = system_length / ngrid
+    c = velocity * dt / dx
+    while True:
+        yield f
+        f = lax_wendroff(f, c)
