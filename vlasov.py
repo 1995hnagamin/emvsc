df2a0912e0fe78aa8993a87af41ea007c9b626f0
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-07-22 23:11:40 +0900

    Move the point of declaration of eps

--- vlasov.py
+++ vlasov.py
@@ -7,9 +7,9 @@ def lax_wendroff(u, courant):
         + courant**2/2 * (np.roll(u, -1) - 2*u + np.roll(u, 1))
 
 def flux_limited_lax_wendroff(limiter):
+    eps = 1e-100 # avoid zero division
     def lw(u, courant):
         diff = np.roll(u,-1) - u # diff[i] = u[i+1] -u[i]
-        eps = 1e-100 # avoid zero division
         p = limiter(np.divide(np.roll(diff, 1), diff+eps)) #  p[i] = (u[i]-u[i-1])/(u[i+1]-u[i])
         F = u + p * (1-courant)/2 * diff
         return u + courant * (np.roll(F,1) - F)
