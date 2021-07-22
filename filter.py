cf2c9d70a63649ada38779281d4eb4af936b902d
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-07-22 21:28:24 +0900

    Add more limiter functions

--- filter.py
+++ filter.py
@@ -1,4 +1,10 @@
 import numpy as np
 
+def minmod(r):
+    return np.clip(r, 0, 1)
+
 def superbee(r):
     return np.maximum(0, np.maximum(np.minimum(2*r, 1), np.minimum(r, 2)))
+
+def vanleer(r):
+    return (r+np.abs(r))/(1+np.abs(r))
