a085deece24faf2e2a3c416ddf910be256e3d797
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-07-22 23:12:23 +0900

    [WIP] Show Vlasov-Poisson simulation result

--- main.py
+++ main.py
@@ -7,39 +7,64 @@ from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 import matplotlib.animation as mplanim
 
 import vlasov
+
+ni = 1.
+q = 1.
+eps0 = 1.
+m = 1.
+xmax = 20.
+vmax = 10.
+nx = 100
+nv = 100
+dt = 0.001
+
+k = 2 * np.pi / xmax
+amp = 0.3
+w = 1.
+v0 = 3.
+
 class Plot(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
         self.figure = mpl.figure.Figure(figsize=(2, 2))
         self.canvas = FigureCanvasWxAgg(self, -1, self.figure)
-        self.animation = mplanim.FuncAnimation(self.figure, self.plot, interval=40)
+        self.animation = mplanim.FuncAnimation(self.figure, self.plot, interval=1000)
         self.ax = self.figure.add_subplot(111)
+        self.colorbar = None
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.canvas, 1, wx.EXPAND)
         self.SetSizer(sizer)
 
-        resolution = 100
-        self.xmax = 100.
-        self.x = np.linspace(0, self.xmax, resolution)
-        width = 5.
-        center = self.xmax / 2
-        f_init = np.exp(-((self.x-center)/width)**2)
-        values = vlasov.adv1d(
-            system_length=self.xmax,
-            velocity=1.,
+        x = np.linspace(0, xmax, nx)
+        v = np.linspace(-vmax, vmax, nv)
+        xx, vv = np.meshgrid(x, v, sparse=True)
+        gamma = ni / (2*np.sqrt(2*np.pi))
+        f_init = gamma*np.exp(-(vv-v0)**2/(2*w**2))*(1+amp*np.cos(k*xx)) \
+                +gamma*np.exp(-(vv+v0)**2/(2*w**2))*(1+amp*np.cos(k*xx))
+        self.x = x
+        self.v = v
+
+        values = vlasov.vp2d(
+            q=q,
+            m=m,
+            ion_density=ni,
+            system_length=xmax,
+            vmax=vmax,
             init=f_init,
-            ngrid=resolution,
-            dt=.1)
-        self.gen = itertools.islice(values, 0, None, 20)
+            ngridx=nx,
+            ngridv=nv,
+            dt=dt)
+        self.gen = itertools.islice(values, 0, None, 1)
 
     def plot(self, _):
         f = next(self.gen)
-
+        self.figure.clf()
         self.ax.cla()
-        self.ax.set_xlim(0, self.xmax)
-        self.ax.set_ylim(-0.5, 1.5)
-        self.ax.plot(self.x, f)
+
+        im = self.ax.imshow(f, cmap='plasma')
+        self.figure.colorbar(im, cax=self.ax)
+        self.figure.show()
 
 if __name__ == "__main__":
     app = wx.App()
