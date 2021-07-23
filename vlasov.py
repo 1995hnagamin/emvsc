e376709adce03d61505958d096da6a6a0b408474
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-07-23 12:48:26 +0900

    Introduce scheme.py

--- vlasov.py
+++ vlasov.py
@@ -1,34 +1,13 @@
 import numpy as np
 import limiter
-
-
-def lax_wendroff(u, courant):
-    return (
-        u
-        - courant / 2 * (np.roll(u, -1) - np.roll(u, 1))
-        + courant ** 2 / 2 * (np.roll(u, -1) - 2 * u + np.roll(u, 1))
-    )
-
-
-def flux_limited_lax_wendroff(limiter):
-    eps = 1e-100  # avoid zero division
-
-    def lw(u, courant):
-        diff = np.roll(u, -1) - u  # diff[i] = u[i+1] -u[i]
-        p = limiter(
-            np.divide(np.roll(diff, 1), diff + eps)
-        )  #  p[i] = (u[i]-u[i-1])/(u[i+1]-u[i])
-        F = u + p * (1 - courant) / 2 * diff
-        return u + courant * (np.roll(F, 1) - F)
-
-    return lw
+import scheme
 
 
 def adv1d(*, system_length, velocity, init, ngrid, dt):
     u = init.copy()
     dx = system_length / ngrid
     c = velocity * dt / dx
-    advance = flux_limited_lax_wendroff(limiter.superbee)
+    advance = scheme.flux_limited_lax_wendroff(limiter.superbee)
     while True:
         yield u
         u = advance(u, c)
@@ -48,7 +27,7 @@ def vp2d(*, q, m, ion_density, system_length, vmax, init, ngridx, ngridv, dt):
     dx = system_length / ngridx
     v = np.linspace(-vmax, vmax, ngridv, endpoint=False)
     dv = 2 * vmax / ngridv
-    advance = flux_limited_lax_wendroff(limiter.superbee)
+    advance = scheme.flux_limited_lax_wendroff(limiter.superbee)
     eps0 = 1.0
 
     A = np.linalg.pinv(divergence_matrix(ngridx, dx))
