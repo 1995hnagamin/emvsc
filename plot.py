Fix calculation method of velocity distribution

plot.py
@@ -40,19 +40,25 @@ class LinePlot:
 
 
 class VelocityDistPlot:
-    def __init__(self, axes, vvalue, species):
+    def __init__(self, axes, vvalue, species, dx):
         self.axes = axes
         self.v = vvalue
         self.species = species
+        self.dx = dx
 
     def init_axes(self, f):
         self.axes.set_prop_cycle(None)
         for s, species in enumerate(self.species):
-            g = f[s].sum(axis=1)
+            g = f[s].sum(axis=1) * self.dx
             self.axes.plot(self.v, g, label=species.name, linewidth=0.3)
         if len(self.species) > 1:
             f_total = f.sum(axis=0)
-            self.axes.plot(self.v, f_total.sum(axis=1), color="black", label="total")
+            self.axes.plot(
+                self.v,
+                f_total.sum(axis=1) * self.dx,
+                color="black",
+                label="total",
+            )
             self.axes.legend()
 
     def plot(self, f, *, show=True):
@@ -62,11 +68,16 @@ class VelocityDistPlot:
             line.remove()
         self.axes.set_prop_cycle(None)
         for s, species in enumerate(self.species):
-            g = f[s].sum(axis=1)
+            g = f[s].sum(axis=1) * self.dx
             self.axes.plot(self.v, g, label=species.name, linewidth=0.3)
         if len(self.species) > 1:
             f_total = f.sum(axis=0)
-            self.axes.plot(self.v, f_total.sum(axis=1), color="black", label="total")
+            self.axes.plot(
+                self.v,
+                f_total.sum(axis=1) * self.dx,
+                color="black",
+                label="total",
+            )
             self.axes.legend()
 
 